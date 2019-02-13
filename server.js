e379694313602b069907ba7cc4a025af53345039
Michael Preston <michael.n.preston@gmail.com>
2019-02-13 13:30:47 -0600

    created public folder

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const path = require('path');
 
 const PORT = 8080;
 const layoutsDir = path.join(__dirname, 'views/mainLayouts'); //Doing this in case I forget how.
+const partialsDir = path.join(__dirname, 'views/pieces');
 
 var app = express();
 app.engine('handlebars', expbs({
     defaultLayout: 'main',
-    layoutsDir
+    layoutsDir,
+    partialsDir,
 }));
 app.set('view engine', 'handlebars');
 
