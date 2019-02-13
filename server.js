36c0a62758245c668f459f974ead10be57f6b995
Michael Preston <michael.n.preston@gmail.com>
2019-02-13 13:57:15 -0600

    fixed issue with app not pointing to /public

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const expbs = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
 const layoutsDir = path.join(__dirname, 'views/mainLayouts'); //Doing this in case I forget how.
 const partialsDir = path.join(__dirname, 'views/pieces');
 
 var app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
 app.engine('handlebars', expbs({
     defaultLayout: 'main',
     layoutsDir,
