e87fd4bebcdba511b04029ba59a73a90189ebddd
Michael Preston <michael.n.preston@gmail.com>
2019-02-23 14:02:48 -0600

    uppercased

--- models/article.js
+++ models/article.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-    //comment
     // pkey: {
     //     type: String,
     //     unique: true
